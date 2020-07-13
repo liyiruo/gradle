d43a0d74f1ad5c9a4672a9c4dbc2145f00b305c1
liyiruo <401213245@qq.com>
2020-07-13 20:21:19 +0800

    Initial commit

---
+++ src/main/java/Test1.java
@@ -0,0 +1,13 @@
+import okhttp3.OkHttpClient;
+
+/**
+ * @author liyiruo
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        //OkHttpClient okHttpClient=new OkHttpClient();
+    }
+
+
+}
